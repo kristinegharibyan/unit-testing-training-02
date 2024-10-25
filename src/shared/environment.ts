5a4ae212eb753eccae6b9d20d02726c7b3a34ee4
Armen Vardanyan <armenvardanyan95@gmail.com>
2024-10-25 17:10:06 +0400

    change API key

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -2,6 +2,6 @@ import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
 export class Environment {
-  apiKey = '758a9675794b45559b982948241507';
+  apiKey = 'a7de796248de4731bb2130937242510';
   apiUrl = 'http://api.weatherapi.com/v1';
 }
